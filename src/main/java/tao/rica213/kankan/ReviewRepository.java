974f10a4d8533e80baa1ae358367f9f06beef823
rica213 <25f5k252l@mozmail.com>
2023-08-25 22:54:16 +0300

    Create endpoint that create reviews

---
+++ src/main/java/tao/rica213/kankan/ReviewRepository.java
@@ -0,0 +1,9 @@
+package tao.rica213.kankan;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
