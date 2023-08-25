5a21c694d08491f07fd91b9e5815f08e229a2d6c
rica213 <25f5k252l@mozmail.com>
2023-08-25 22:51:56 +0300

    Create the main test class

---
+++ src/test/java/tao/rica213/kankan/KankanApplicationTests.java
@@ -0,0 +1,13 @@
+package tao.rica213.kankan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KankanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
