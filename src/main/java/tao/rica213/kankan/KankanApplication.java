76ea468b254b9e908b51082116163c63bac3744d
rica213 <25f5k252l@mozmail.com>
2023-08-25 22:51:23 +0300

    Create the main class

---
+++ src/main/java/tao/rica213/kankan/KankanApplication.java
@@ -0,0 +1,12 @@
+package tao.rica213.kankan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KankanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KankanApplication.class, args);
+	}
+}
